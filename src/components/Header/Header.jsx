90de57ec439e3c5f5606fcdf6c41eb0c8571b229
Reeshabh Jain <rishabhrox2001@gmail.com>
2022-01-25 19:38:19 +0530

    Responsive Timer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,10 +13,14 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/team">Team</Nav.Link>
-              <Nav.Link href="/activities">Activities</Nav.Link>
-              <Nav.Link href="/contact">Contact Us</Nav.Link>
+              <Nav.Link
+                href="/">Home</Nav.Link>
+              <Nav.Link
+                href="/team">Team</Nav.Link>
+              <Nav.Link
+                href="/activities">Activities</Nav.Link>
+              <Nav.Link
+                href="/contact">Contact Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
